9af1e09a55a9c16bd1c1691c7b3b66784e0dcaa4
shreyapurohit <shreyapurohit94@gmail.com>
2021-10-05 16:27:36 +0530

    Update factorial.cpp
    Added recursive approach to calculate factorial

--- CPP/factorial.cpp
+++ CPP/factorial.cpp
@@ -1,44 +1,19 @@
-
 #include <iostream>
-
 using namespace std;
-
-int multiply(int x, int a[], int size)
+ 
+// function to find factorial of given number
+unsigned int factorial(unsigned int n)
 {
-    int carry = 0, i, p;
-
-    for (i = 0; i < size; ++i)
-    {
-        p = a[i] * x + carry;
-        a[i] = p % 10;
-        carry = p / 10;
-    }
-    while (carry != 0)
-    {
-        a[size] = carry % 10;
-        carry = carry / 10;
-        size++;
-    }
-    return size;
+    if (n == 0)
+        return 1;
+    return n * factorial(n - 1);
 }
-
+ 
+// Driver code
 int main()
 {
-    int n, a[1000], i, size = 1;
-    a[0] = 1;
-
-    cout << "Enter any large number:";
-    cin >> n;
-
-    for (i = 2; i <= n; ++i)
-    {
-        size = multiply(i, a, size);
-    }
-
-    for (i = size - 1; i >= 0; --i)
-    {
-        cout << a[i];
-    }
-
+    int num = 5;
+    cout << "Factorial of "
+         << num << " is " << factorial(num) << endl;
     return 0;
 }
